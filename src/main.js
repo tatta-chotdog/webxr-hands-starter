e5a3b6ad91fce00d76af2784d946ce10ffc08765
tatta-chotdog <tatta875dev@gmail.com>
2025-02-28 22:36:27 +0900

    feature: open and close

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
+import * as THREE from "three";
 import { SceneManager } from "./utils/SceneManager.js";
 import { ControllerManager } from "./utils/ControllerManager.js";
 
 let sceneManager, controllerManager;
-let lastGesture = "";
-let color = 0x00ffff;
-let mesh;
+
+const createTrackedHand = () => ({
+  mesh: new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  ),
+  color: 0x00ff00,
+  lastGesture: "",
+});
+
+const trackedHands = [createTrackedHand(), createTrackedHand()];
 
 const init = () => {
   sceneManager = new SceneManager();
@@ -14,6 +23,12 @@ const init = () => {
     sceneManager.renderer,
     sceneManager.scene
   );
+
+  trackedHands[0].mesh.position.set(-1, 0, -4);
+  sceneManager.scene.add(trackedHands[0].mesh);
+
+  trackedHands[1].mesh.position.set(1, 0, -4);
+  sceneManager.scene.add(trackedHands[1].mesh);
 };
 
 const render = (_, xrFrame) => {
@@ -23,8 +38,8 @@ const render = (_, xrFrame) => {
 
     const referenceSpace = sceneManager.renderer.xr.getReferenceSpace();
 
-    for (const inputSource of session.inputSources) {
-      if (inputSource.hand && inputSource.handedness === "right") {
+    for (const [index, inputSource] of session.inputSources.entries()) {
+      if (inputSource.hand) {
         const wristJoint = inputSource.hand.get("wrist");
         const indexTipJoint = inputSource.hand.get("index-finger-tip");
 
@@ -43,13 +58,16 @@ const render = (_, xrFrame) => {
             wristPose.transform.position.z - indexTipPose.transform.position.z;
           const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
 
-          const threshold = 0.15;
+          const threshold = 0.1;
           const gesture = distance > threshold ? "OPEN" : "CLOSE";
 
-          if (gesture !== lastGesture) {
-            lastGesture = gesture;
-            color = gesture === "OPEN" ? 0x00ff00 : 0xff0000;
-            // mesh.material.color.set(color);
+          if (gesture !== trackedHands[index].lastGesture) {
+            trackedHands[index].color =
+              gesture === "OPEN" ? 0x00ff00 : 0xff0000;
+            trackedHands[index].mesh.material.color.set(
+              trackedHands[index].color
+            );
+            trackedHands[index].lastGesture = gesture;
           }
         }
       }
